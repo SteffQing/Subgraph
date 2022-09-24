59ca6ea712f17ce0079fce267364ee002235dbeb
Data Nexus <derek.m.meyer@gmail.com>
2022-09-24 11:19:51 -0400

    Update mARTMapping.ts
    Updated Mappings

--- src/marketplaces/mARTMapping.ts
+++ src/marketplaces/mARTMapping.ts
@@ -2,7 +2,6 @@ import {
 	sale,
 	transaction,
   currency,
-  wethTransaction,
 } from '../../generated/schema'
 
 import {
@@ -34,16 +33,9 @@ export function handleItemSale(event: ItemListed): void {
     let saleEntity = sale.load(event.block.number.toString() + '-' + event.logIndex.toString())
     if (!saleEntity && tx.unmatchedTransferCount > 0) {
 
-      // Assume default value of currency as phony ERC20
+      // Assume default value of currency as Daz
       let currencyAddress: Address = Address.fromString('0xf75150d730ce97c1551e97df39c0a049024e4c25') //update to unknown ERC20
       
-      let wethTest = wethTransaction.load(event.transaction.hash.toHexString())
-      
-      // If there was a transfer of WETH assume the sale occurs in WETH
-      if (wethTest) { currencyAddress = Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')}
-      //If the transaction value is > 0 then assume the sale occurs in ETH
-      if (event.transaction.value != constants.BIGINT_ZERO || event.params.price == constants.BIGINT_ONE) {currencyAddress = Address.fromString(constants.ADDRESS_ZERO)}
-
       ERC20Contracts.getERC20(currencyAddress)
       let currencyEntity = currency.load(currencyAddress.toHexString())
         
